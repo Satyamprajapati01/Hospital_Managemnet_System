2e9d5be9c45ceb01be0e28b7703612a2bb6f9f63
Satyam <satyam731835@gmail.com>
2026-07-24 00:09:04 +0530

    Add files via upload

---
+++ text.java
@@ -0,0 +1,17 @@
+package hospital.management.system;
+
+import javax.swing.*;
+
+public class text extends JFrame {
+    text(){
+        setSize(500,500);
+        setVisible(true);
+
+    }
+
+
+    public static void main (String[]  args){
+        new text();
+
+    }
+}
